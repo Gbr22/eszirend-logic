cbd0275191637e042f18c0fcdaa1014e38e99341
Gábor Kriskó <gabor.krisko@gmail.com>
2021-04-22 19:48:20 +0200

    add files to repo

---
+++ objects.js
@@ -0,0 +1,259 @@
+export function getList(data, id){
+    return data.r.dbiAccessorRes.tables.find(e=>e.id == id);
+}
+export function getListItem(data,list,id){
+    return getList(data,list).data_rows.find(e=>e.id == id);
+}
+export class Subject {
+
+    data;
+    json;
+
+    name;
+    shortName;
+    color;
+    id;
+
+    constructor(data,json){
+        this.data = data;
+        this.json = json;
+
+        this.name = json.name;
+        this.shortName = json.short;
+        this.id = json.id;
+        this.color = json.color;
+    }
+}
+export class Lesson {
+
+    data;
+    json;
+
+    classIds;
+    id;
+    teacherIds;
+    teachers;
+    durationPeriods = 1;
+    subject;
+    color;
+    groups;
+
+    constructor(data,json){
+        this.data = data;
+        this.json = json;
+
+        this.classIds = this.json.classids;
+        this.id = this.json.id;
+        this.teacherIds = json.teacherids;
+        this.teachers = this.teacherIds.map(e=>new Teacher(data,getListItem(data.json,"teachers",e)));
+        this.durationPeriods = json.durationperiods;
+        this.subject = new Subject(this.data,getListItem(this.data.json,"subjects",this.json.subjectid));
+        this.groups = this.json.groupids.map(e=>{
+            return new Group(this.data,getListItem(this.data.json,"groups",e));
+        });
+        this.color = this.groups[0]?.color;
+    }
+}
+export class Entry {
+
+    data;
+    json;
+
+    lesson;
+    period;
+
+    classrooms = [];
+
+    get periods(){
+        let arr = [];
+        for (let i = 0; i < this.lesson.durationPeriods; i++) {
+            arr.push((parseInt(this.period)+i).toString());
+        }   
+        return arr;
+    }
+
+    constructor(data,json){
+        this.data = data;
+        this.json = json;
+        this.days = json.days;
+
+        let el = getListItem(this.data.json,"lessons",this.json.lessonid);
+        /* console.log("el",el); */
+
+        this.lesson = new Lesson(this.data,el);
+        this.period = json.period;
+        this.classrooms = json.classroomids.map(e=>new Classroom(this.data,getListItem(this.data.json,"classrooms",e)))
+    }
+}
+export class Classroom {
+
+    data;
+    json;
+
+    id;
+    name;
+    shortName;
+
+    constructor(data,json){
+        this.data = data;
+        this.json = json;
+
+        this.id = json.id;
+        this.name = json.name;
+        this.shortName = json.short;
+    }
+}
+
+function generateTimetable(data,classId = "-40"){
+
+    let cards = getList(data,"cards").data_rows.map(e=>new Entry(data,e));
+    
+    cards = cards.filter(e=>e.lesson.json.classids.includes(classId));
+    
+    let days = {};
+    cards.forEach(e=>{
+        if (!days[e.day.id]){
+            days[e.day.id] = {
+                info:e.day,
+                lessons:[],
+            };
+        }
+        days[e.day.id].lessons.push(e);
+    })
+}
+export class Day {
+
+    data;
+    json;
+    
+    vals;
+    val;
+    name;
+    shortName;
+    id;
+
+    matches(v){
+        return this.vals.includes(v);
+    }
+
+    constructor(data,json){
+        this.data = data;
+        this.json = json;
+        this.vals = json.vals;
+        this.val = json.val;
+        this.shortName = json.short;
+        this.name = json.name;
+        this.id = json.id;
+    }
+}
+export class Class {
+
+    data;
+    json;
+    
+    id;
+    name;
+    shortName;
+    color;
+
+    constructor(data,json){
+        this.data = data;
+        this.json = json;
+
+        this.id = json.id;
+        this.name = json.name;
+        this.shortName = json.short;
+        this.color = json.color;
+    }
+}
+export class Teacher {
+
+    data;
+    json;
+    name;
+    id;
+
+    constructor(data,json){
+        this.data = data;
+        this.json = json;
+
+        this.name = json.short;
+        this.id = json.id;
+    }
+}
+export class Group {
+
+    data;
+    json;
+
+    color;
+    entireClass;
+    name;
+    divisionId;
+    id;
+
+    constructor(data,json){
+        this.data = data;
+        this.json = json;
+        this.color = json.color;
+        this.entireClass = json.entireclass;
+        this.name = json.name;
+        this.divisionId = json.divisionid;
+        this.id = json.id;
+    }
+}
+export class Period {
+
+    data;
+    json;
+
+    name;
+    id;
+    period;
+    startTime;
+    endTime;
+
+    constructor(data,json){
+        this.data = data;
+        this.json = json;
+
+        this.name = json.name;
+        this.id = json.id;
+        this.period = json.period;
+        this.startTime = json.starttime;
+        this.endTime = json.endtime;
+    }
+}
+export class Info {
+    data;
+
+    headerText;
+    validityText;
+
+    constructor(data,json){
+        this.data = data;
+
+        this.headerText = json.settings.m_strPrintHeaderText;
+        this.validityText = json.settings.m_strDateBellowTimeTable;
+    }
+}
+export class DataRoot {
+    periods;
+
+    json;
+
+    classes = [];
+    days = [];
+    info;
+
+    constructor(json){
+        this.json = json;
+        let periods = getList(this.json,"periods").data_rows.map(e=>new Period(this,e));
+        this.periods = periods;
+
+        this.classes = getList(this.json,"classes").data_rows.map(e=>new Class(this,e));
+        this.days = getList(this.json,"daysdefs").data_rows.map(e=>new Day(this,e));
+        this.entries = getList(this.json,"cards").data_rows.map(e=>new Entry(this,e));
+        this.info = new Info(this,getList(this.json,"globals").data_rows[0]);
+    }
+}
