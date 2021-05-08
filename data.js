2865f625ffbcc1af21135134cc993d8a4fa0446c
Gábor Kriskó <gabor.krisko@gmail.com>
2021-05-08 17:48:30 +0200

    use commonjs

--- data.js
+++ data.js
@@ -1,6 +1,4 @@
-import fetch from 'node-fetch';
-
-export function getData(){
+module.exports.getData = function(fetch){
     return fetch("https://eszi.edupage.org/timetable/server/regulartt.js?__func=regularttGetData",{
         method:"POST",
         body:`{"__args":[null,"74"],"__gsh":"00000000"}`,
